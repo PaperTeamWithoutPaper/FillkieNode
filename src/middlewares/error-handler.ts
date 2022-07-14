9f6100e7ad8662ce298baafe7e1d1068f055c664
fienestar <fienestar@gmail.com>
2022-07-14 18:17:34 +0900

    refact: improve readability using response utility

--- src/middlewares/error-handler.ts
+++ src/middlewares/error-handler.ts
@@ -1,27 +1,19 @@
 import {Request, Response, NextFunction} from 'express';
+import {responseError} from '../utils';
 
 /**
  * error handler
  *
  * @param {Error} err error from express
- * @param {Request} _req request
+ * @param {Request} req request
  * @param {Response} res response
- * @param {NextFunction} _next next function
  */
 export default function handleError(
     err: Error,
-    _req: Request,
+    req: Request,
     res: Response,
-    // eslint-disable-next-line @typescript-eslint/no-unused-vars
-    _next: NextFunction,
 ) {
-    console.error(err);
 
-    res
-        .status(500)
-        .json({
-            success: false,
-            code: 500,
-            message: 'Internal Server Error',
-        });
+    console.error(req, err);
+    responseError(res, 500);
 }
