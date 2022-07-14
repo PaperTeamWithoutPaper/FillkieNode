c8c76756306c882e171f907d560aaca3e7009181
fienestar <fienestar@gmail.com>
2022-07-14 18:19:05 +0900

    feat: add project model

---
+++ src/model/Project.ts
@@ -0,0 +1,28 @@
+import mongoose, {Schema} from 'mongoose';
+
+type ObjectId = mongoose.Types.ObjectId;
+
+export interface IProject {
+    _id: ObjectId,
+    name: string,
+    expired: boolean,
+    ownerId: ObjectId,
+    teamId: ObjectId,
+    roles: {
+        [groupId: string]: string[],
+    }
+}
+
+const projectSchema = new Schema<IProject>({
+    _id: mongoose.Types.ObjectId,
+    name: String,
+    expired: Boolean,
+    ownerId: mongoose.Types.ObjectId,
+    teamId: mongoose.Types.ObjectId,
+    roles: {
+        String: [String],
+    },
+});
+
+mongoose.pluralize(null);
+export default mongoose.model('project', projectSchema);
