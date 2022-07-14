fc942d8017527fa48f11a7834d4e6ef4ce45d253
fienestar <fienestar@gmail.com>
2022-07-14 18:15:23 +0900

    feat: add utility for response

---
+++ src/utils.ts
@@ -0,0 +1,46 @@
+import {Response} from 'express';
+import {STATUS_CODES} from 'http';
+
+export type responseJson = {
+    success: boolean,
+    code: number,
+    message: string,
+    [key: string]: string | number | boolean,
+}
+
+/**
+ * response error
+ * @param {Response} res
+ * @param {number} statusCode
+ * @param {string | undefined} message
+ */
+export function responseError(res: Response, statusCode: number, message?: string) {
+    const json: responseJson = {
+        success: false,
+        code: statusCode,
+        message: message ?? STATUS_CODES[statusCode] as string,
+    };
+
+    res
+        .status(statusCode)
+        .json(json);
+}
+
+
+/**
+ * response success
+ * @param {Response} res
+ * @param {string | undefined} message
+ */
+export function responseSuccess(res: Response, message?: string) {
+    const statusCode = 200;
+    const json: responseJson = {
+        success: true,
+        code: statusCode,
+        message: message ?? STATUS_CODES[statusCode] as string,
+    };
+
+    res
+        .status(statusCode)
+        .json(json);
+}
