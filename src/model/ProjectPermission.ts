ce212de8f10af084994cdde72b3c6f279981fa25
fienestar <fienestar@gmail.com>
2022-07-18 15:03:03 +0900

    feat: add project permission schema

---
+++ src/model/ProjectPermission.ts
@@ -0,0 +1,20 @@
+import mongoose, {Schema} from 'mongoose';
+
+type ObjectId = mongoose.Types.ObjectId;
+const ObjectId = mongoose.Types.ObjectId;
+
+export interface IProjectPermission {
+    _id: ObjectId,
+    projectId: ObjectId
+    groupId: ObjectId,
+    permissionCode: number,
+}
+
+const projectPermissionSchema = new Schema<IProjectPermission>({
+    projectId: ObjectId,
+    groupId: ObjectId,
+    permissionCode: Number,
+});
+
+mongoose.pluralize(null);
+export default mongoose.model('ProjectPermission', projectPermissionSchema);
