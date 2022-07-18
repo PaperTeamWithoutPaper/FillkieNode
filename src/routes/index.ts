2a981059eebaedb7fbc888bcce4a93cdd89d8ffc
fienestar <fienestar@gmail.com>
2022-07-18 17:26:37 +0900

    feat: add mock /user/oauth/google api

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,10 +1,12 @@
 import express from 'express';
+import userRouter from './user';
 import dirRouter from './dir';
 import fileRouter from './file';
 import teamRouter from './team';
 
 const router = express.Router();
 
+router.use('/user', userRouter);
 router.use('/dir', dirRouter);
 router.use('/file', fileRouter);
 router.use('/team', teamRouter);
