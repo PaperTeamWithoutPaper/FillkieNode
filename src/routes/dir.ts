289ecaad9f3e90da8d3736d8b23c97fe6823f3a2
fienestar <fienestar@gmail.com>
2022-07-25 00:11:31 +0900

    feat: folder create api

--- src/routes/dir.ts
+++ src/routes/dir.ts
@@ -1,20 +1,54 @@
 import express from 'express';
+import {drive_v3 as DriveV3} from 'googleapis';
+import GoogleDriveException from '../exceptions/google_drive_exception';
+import {asyncHandler} from '../middlewares/async_handler';
 import auth from '../middlewares/auth';
-import {initializeGoogleApi} from '../middlewares/google_drive';
+import {initializeGoogleApi, realCreateType, RequestWithGoogleDrive} from '../middlewares/google_drive';
+import {AssertedHeader, requireBody, requireQuery} from '../middlewares/requires';
+import MIME_TYPE_MAP from '../mime_type_map';
+import {FILLKIE_STATUS_MESSAGES, responseError} from '../utils';
+import {isGoogleDriveFileId, isMongoId} from '../validators';
 
 const router = express.Router();
 
 router.use(auth);
-router.use(initializeGoogleApi);
 
-router.post('/', (req, res) => {
-    console.log('post:', req.body);
+router.post('/', requireBody({
+    name: String,
+    projectId: isMongoId,
+    folderId: isGoogleDriveFileId,
+}), initializeGoogleApi, asyncHandler(async (req, res) => {
+    // TODO: check parent folder in project folder
+    // TODO: check user has permission to create folder in this project
+
+    const body = req.body as AssertedHeader;
+    const folderId = body.folderId;
+    const drive = (req as RequestWithGoogleDrive).drive;
+
+    try {
+        const newFolder = await(drive.files.create as realCreateType)({
+            fields: 'id',
+            resource: {
+                name: body.name,
+                title: 'title of fillkie',
+                mimeType: 'application/vnd.google-apps.folder',
+                parents: [folderId],
+            },
+        });
+
+        if (newFolder.data.id === undefined) {
+            throw new Error(newFolder.statusText);
+        }
+    } catch (e) {
+        throw new GoogleDriveException((e as Error).toString());
+    }
+
     res.json({
         success: true,
         code: 200,
         message: 'success',
     });
-});
+}));
 
 router.get('/', (req, res) => {
     console.log('get:', req.body);
