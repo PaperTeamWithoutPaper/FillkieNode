f87d3eebf3f7bf7046fac3774ef71bff0d5c4520
fienestar <fienestar@gmail.com>
2022-07-25 14:59:23 +0900

    feat: delete ownerId in group schema

--- src/model/group.ts
+++ src/model/group.ts
@@ -6,13 +6,11 @@ const ObjectId = mongoose.Types.ObjectId;
 export interface IGroup {
     _id: ObjectId,
     name: string,
-    ownerId: ObjectId,
     teamId: ObjectId,
 }
 
 const groupSchema = new Schema<IGroup>({
     name: String,
-    ownerId: ObjectId,
     teamId: ObjectId,
 });
 
