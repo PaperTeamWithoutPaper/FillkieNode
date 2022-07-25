ad6864e636a3ca01c966278b785b1952ea91c56e
fienestar <fienestar@gmail.com>
2022-07-25 16:12:33 +0900

    fix: #5 handle body parser exception

--- src/middlewares/error_handler.ts
+++ src/middlewares/error_handler.ts
@@ -10,12 +10,14 @@ import {FILLKIE_STATUS_MESSAGES, responseError} from '../utils';
  * @param {Response} res response
  */
 export default function handleError(
-    err: Error | GoogleDriveException,
+    err: (Error | GoogleDriveException) & Partial<{ statusCode: number, expose: boolean }>,
     req: Request,
     res: Response,
 ) {
     if (err instanceof GoogleDriveException) {
         responseError(res, FILLKIE_STATUS_MESSAGES.GOOGLE_DRIVE_ERROR, err.message);
+    } else if (err.statusCode !== undefined && err.expose) {
+        responseError(res, err.statusCode);
     } else {
         console.error(req.url, req.ip, err);
         responseError(res, 500);
