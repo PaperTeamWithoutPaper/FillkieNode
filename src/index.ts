cc5558f7d080dbdb4fbc9ae750cd3cb873b3051b
fienestar <fienestar@gmail.com>
2022-08-13 05:45:17 +0900

    refact: seperate app, main script

--- src/index.ts
+++ src/index.ts
@@ -2,11 +2,7 @@ import express, {NextFunction, Request, Response} from 'express';
 import mongoose from 'mongoose';
 import process from 'process';
 import dotenv from 'dotenv';
-import compression from 'compression';
-import helmet from 'helmet';
-import routes from './routes';
-import errorHandler from './middlewares/error_handler';
-import cors from 'cors';
+import app from './app';
 
 /**
  * start app
@@ -17,18 +13,6 @@ async function startApp(): Promise<void> {
 
     const mongoURI = process.env.MONGO_CONNECTION_URI || 'mongodb://localhost:27017/test';
     await mongoose.connect(mongoURI);
-
-    const app = express();
-
-    app.use(compression());
-    app.use(helmet());
-    app.use(express.json());
-    app.use(express.urlencoded({extended: true}));
-    app.use(cors());
-    app.use('/node', routes);
-    app.use((err: Error, req: Request, res: Response, _next: NextFunction) => {
-        errorHandler(err, req, res);
-    });
 
     const port = Number(process.env.PORT || 8080);
     app.listen(port, () => {
